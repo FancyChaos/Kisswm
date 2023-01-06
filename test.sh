35f75a0727fb7d31b51ab5d887fbae9aaab6a732
FancyChaos <Felixs.Developer@tutanota.com>
2023-01-06 16:13:38 +0100

    Upate test script to fit current resolution

--- test.sh
+++ test.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+which Xephyr >/dev/null 2>&1 || { echo "Missing application - Xephyr" 1>&2; exit 1; }
+
+RES=$(xrandr | awk 'NR==2 { print $4 }')
+
 M=make
 [  $(uname) = OpenBSD ] && M=gmake
 
 $M clean
 $M
 
-Xephyr -br -ac -reset -screen 1920x1080 :1 &
+Xephyr -br -ac -reset -screen $RES :1 &
 export DISPLAY=:1
 sleep 2
 
